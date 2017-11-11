0e9c5f85ee00b8e826048d98118eb9e1d40f0894
Anthony Leif <tonyleif@hotmail.com>
2017-11-11 15:25:51 -0600

    Page 556

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+            string message = "This is an HTML element: <input>";
+            return View((object)message);
+        }
+    }
+}
