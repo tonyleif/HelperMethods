3e8b5212586da98a64570d83c4c0b1efcd3b3a26
Anthony Leif <tonyleif@hotmail.com>
2017-11-12 21:35:45 -0600

    Page 612

---
+++ HelperMethods/Controllers/PeopleController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using HelperMethods.Models;
+using System.Collections.Generic
+
+namespace HelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person{FirstName="Adam", LastName="Freeman", Role=Role.Admin},
+            new Person {FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User},
+            new Person {FirstName = "John", LastName = "Smith", Role = Role.User},
+            new Person {FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+        };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult GetPeopleData(string selectedRole = "All")
+        {
+            IEnumerable<Person> data = personData;
+            if(selectedRole != "All")
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                data = personData.Where(p => p.Role == selected);
+            }
+            return PartialView(data);
+        }
+
+        public ActionResult GetPeople(string selectedRole = "All")
+        {
+            return View((object)selectedRole);
+        }
+
+        public ActionResult GetPeople()
+        {
+            return View(personData);
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(string selectedRole)
+        {
+            if(selectedRole == null || selectedRole == "All")
+            {
+                return View(personData);
+            }
+            else
+            {
+                Role selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                return View(personData.Where(p => p.Role == selected));
+            }
+        }
+    }
+}
